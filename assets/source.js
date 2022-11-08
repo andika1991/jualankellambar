262c667e005503f91d2cf4674bbd50dbac5ad673
andika fikri azhari <109733872+andika1991@users.noreply.github.com>
2022-11-08 23:51:54 +0700

    Update source.js

--- assets/source.js
+++ assets/source.js
@@ -2,7 +2,7 @@ document.getElementById("year").innerHTML = new Date().getFullYear();
 
 function buy(product_name, product_price) {
   const message = `Halo saya mau beli ${product_name} - ${product_price}`
-  const whatsapp = `https://wa.me/628971483383?text=${message}`
+  const whatsapp = `https://wa.me/6288973748682?text=${message}`
   open(whatsapp, "_blank")
 }
 
